f243b6af03b16d4e8a538f6a8cc7b5236d320c03
GayaneGulanyan <66916093+GayaneGulanyan@users.noreply.github.com>
2020-06-22 17:10:45 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte a=23;
+	byte b=35;
+	int c=a+b;
+	System.out.println(c);// write your code here
+    }
+}
